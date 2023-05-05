19e700b9d2f71d0faab051b24c9c340f6535afe1
arun_microexcel <Arun.K@microexcel.com>
2023-05-05 16:10:27 +0530

    new changes

--- app.js
+++ app.js
@@ -44,9 +44,9 @@ app.use((err, req, res, next) => {
 
  
 
-
+const PORT = process.env.PORT || 3000
 
 
 
 // start server
-app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
+app.listen(PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
--- models/db.js
+++ models/db.js
@@ -5,7 +5,7 @@ mongoose.set('useFindAndModify', false );
 mongoose.set('useCreateIndex', true)
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect(process.env.MONGODB_URI, (err) => {
+mongoose.connect(process.env.MONGO_URI, (err) => {
     if (!err) { console.log('MongoDB connection succeeded.'); }
     else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
 });
